078e1610a5a3ebcea422d08aea9648a999b4dc7d
josex2r <jlrepresa@gmail.com>
2023-10-05 15:58:38 +0200

    feat: move components to components directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-
-import './App.css';
-import Home from './Home';
-import { Login } from './User';
-import { Routes, Route } from "react-router-dom"
-
+import "./App.css";
+import Home from "./components/Home";
+import { Login } from "./components/User";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-        <Route path="Home" element={ <Home/> } />
-      </Routes>
-        
+          <Route path="Home" element={<Home />} />
+        </Routes>
+
         <Login />
       </header>
     </div>
